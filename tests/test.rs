92ea3fbc2d844a547d137e3282fdc14f05917f00
David Tolnay <dtolnay@gmail.com>
2025-02-02 09:36:10 -0800

    Add integration test

---
+++ tests/test.rs
@@ -0,0 +1,20 @@
+use include_json::include_json;
+use serde_json::json;
+
+#[test]
+fn test() {
+    let actual = include_json!("tests/test.json");
+    let expected = json!({
+        "null": null,
+        "true": true,
+        "false": false,
+        "posint": 10,
+        "negint": -10,
+        "float": 10.0,
+        "string": "...",
+        "emptyarray": [],
+        "array": [1, 2, 3],
+        "emptyobject": {}
+    });
+    assert_eq!(actual, expected);
+}
