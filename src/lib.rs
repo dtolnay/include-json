2840f5f007daf51cb05f765a5be57a88fc1b74a8
David Tolnay <dtolnay@gmail.com>
2025-02-02 10:23:03 -0800

    Add crosslink icons to top of rustdoc

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,11 @@
+//! [![github]](https://github.com/dtolnay/include-json)&ensp;[![crates-io]](https://crates.io/crates/include_json)&ensp;[![docs-rs]](https://docs.rs/include_json)
+//!
+//! [github]: https://img.shields.io/badge/github-8da0cb?style=for-the-badge&labelColor=555555&logo=github
+//! [crates-io]: https://img.shields.io/badge/crates.io-fc8d62?style=for-the-badge&labelColor=555555&logo=rust
+//! [docs-rs]: https://img.shields.io/badge/docs.rs-66c2a5?style=for-the-badge&labelColor=555555&logo=docs.rs
+//!
+//! <br>
+//!
 //! Rust macro to parse a JSON file at compile time and compile it into the
 //! program as a `serde_json::Value`.
 //!
