b0dfbb7b654b8079d8785b59bf96b9dcb87c6618
David Tolnay <dtolnay@gmail.com>
2026-02-24 19:36:04 -0800

    Release 0.1.2

--- src/lib.rs
+++ src/lib.rs
@@ -39,7 +39,7 @@
 //! }
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/include_json/0.1.1")]
+#![doc(html_root_url = "https://docs.rs/include_json/0.1.2")]
 
 use macro_string::MacroString;
 use proc_macro::TokenStream;
